3d991c889c04db200a4555adf334535284526c03
Angel Hernandez <angelH98@outlook.com>
2021-11-28 22:30:18 -0600

    Courses crud

--- school-management/Models/Courses.cs
+++ school-management/Models/Courses.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace school_management.Models
 {
     public class Courses
     {
         public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [Remote(action: "courseNameIsAvailable", controller: "Courses", AdditionalFields = "id", ErrorMessage = "Nombre de curso no disponible")]
         public string coursename { get; set; }
         public string coursestatus { get; set; }
         public int courseyear { get; set; }
